ff083ae954c67dda835897c603000fae42d1f9a4
Mgirijashree <mgirija.shree@gmail.com>
2026-07-11 04:55:12 +0530

    Updated API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-
-    baseURL: "http://127.0.0.1:8000/",
-
+    baseURL: "https://ecommerce-backend.onrender.com/",
     withCredentials: true,
-
 });
 
 export default api;
