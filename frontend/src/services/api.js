b6ce2451d628e07f8f411a56373815be1aabb6b2
Mgirijashree <mgirija.shree@gmail.com>
2026-07-11 09:30:45 +0530

    Fix register and login URL mappings

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://ecommerce-7jru.onrender.com/products/",
+    baseURL: "https://ecommerce-7jru.onrender.com/product/",
 });
 
 export default API;
