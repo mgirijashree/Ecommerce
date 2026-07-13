b13e25250ae793a84ca1088ea2cd67f2015fc796
Mgirijashree <mgirija.shree@gmail.com>
2026-07-13 11:35:08 +0530

    Remove Cloudinary and use Django media storage

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -47,14 +49,11 @@ INSTALLED_APPS = [
 
     'store',
 
-    "cloudinary",
-    "cloudinary_storage",
+    
     
 ]
 
-DEFAULT_FILE_STORAGE = (
-    "cloudinary_storage.storage.MediaCloudinaryStorage"
-)
+
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
