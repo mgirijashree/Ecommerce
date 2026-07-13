0e81684ae2f0cab21b7288ff482dbc0aee97e7e1
Mgirijashree <mgirija.shree@gmail.com>
2026-07-13 12:55:09 +0530

    Fix

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -49,14 +49,7 @@ export default function Header({
 
 
       {/* Global Search */}
-      <div
-        className="
-    w-32
-    sm:w-48
-    md:w-64
-    lg:w-80
-  "
-      >
+      <div className="flex-1">
 
         <input
           type="text"
@@ -73,9 +66,7 @@ export default function Header({
             px-3
             py-2
             text-sm
-            focus:outline-none
-            focus:ring-2
-            focus:ring-blue-500
+            sm:text-base
           "
         />
 
