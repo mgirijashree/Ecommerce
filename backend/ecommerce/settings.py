eed82273a641fefc880785332aa9703c9b0faa88
Mgirijashree <mgirija.shree@gmail.com>
2026-07-25 14:49:59 +0530

    Fix database migrations and ignore venv

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv(
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
